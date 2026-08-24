78db4b6ce04706fc508fb3fb6cb5458c976d1b0f
Sohamayer <sohamayer516@gmail.com>
2026-08-24 20:45:01 +0530

    Made changes in login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,21 +1,5 @@
-"use client";
-
-import { useEffect } from "react";
-import { useSearchParams } from "next/navigation";
-import "./page.css";
+import { redirect } from "next/navigation";
 
 export default function LoginPage() {
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    const redirect = searchParams.get("redirect") || "/";
-    window.location.href = `/api/auth/login?redirect=${encodeURIComponent(redirect)}`;
-  }, [searchParams]);
-
-  return (
-    <div className="login-redirect">
-      <div className="login-spinner" />
-      <p>Redirecting you to secure login…</p>
-    </div>
-  );
+  redirect("/api/auth/login");
 }
